69d017d2a2035d8222071286e1b9263f7ccab0cb
Marie-Claire <mcbalabanian@thisismess.com>
2024-03-10 13:51:45 -0700

    env dir

--- api/hello.php
+++ api/hello.php
@@ -6,7 +6,7 @@ use PHPMailer\PHPMailer\Exception;
 //Load Composer's autoloader
 require 'vendor/autoload.php';
 
-$env = parse_ini_file('../.env');
+$env = parse_ini_file(__DIR__."/.env");
 
 /**
  * The function uses the PHPMailer object to send an email 

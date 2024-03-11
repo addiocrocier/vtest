96052704095df884314eeccbf13c1d0e13c6c6bd
Marie-Claire <mcbalabanian@thisismess.com>
2024-03-11 14:54:24 -0700

    arg

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const express = require("express");
+import express from 'express'
 const app = express();
 
 
-require('dotenv').config()
 const postmark = process.env.POSTMARK;
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
-module.exports = app;
+module.exports = postmark;
